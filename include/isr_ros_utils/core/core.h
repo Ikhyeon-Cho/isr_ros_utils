c653b4f10182a468ce60275d1d6e1d3b3c135329
Ikhyeon Cho <tre0430@korea.ac.kr>
2023-11-30 18:23:57 +0900

    rename(header-files): Change from utils.h to core.h, Add tools.h
    Usage: #include <isr_ros_utils/core/core.h>
           #include <isr_ros_utils/tools/tools.h>
    
    Core contains ROS node related things.
    Tools contains TransformHandler, PointcloudProcessor, MsgConverter

--- include/isr_ros_utils/core/core.h
+++ include/isr_ros_utils/core/core.h
@@ -1,5 +1,5 @@
-#ifndef ISR_ROS_UTILS_CORE_UTILS
-#define ISR_ROS_UTILS_CORE_UTILS
+#ifndef ISR_ROS_UTILS_CORE_CORE
+#define ISR_ROS_UTILS_CORE_CORE
 
 #include "isr_ros_utils/core/Parameter.h"
 #include "isr_ros_utils/core/Publisher.h"
